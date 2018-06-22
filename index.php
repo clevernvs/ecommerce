bff219a81db387668cb608a547ea86ab1b8f330f
Cleverton Neves <c.neves88@gmail.com>
2018-06-22 19:08:25 -0300

    Tela de login

--- index.php
+++ index.php
@@ -1,15 +1,19 @@
 <?php
 
+session_start();
+
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new Slim();
 
 $app->config('debug', true);
 
+// Rota p/ o Site
 $app->get('/', function() {
 
     $page = new Page();
@@ -18,12 +22,46 @@ $app->get('/', function() {
 
 });
 
+// Rota p/ o Administrador
 $app->get('/admin', function() {
+
+    User::verifyLogin();
 
     $page = new PageAdmin();
 
     $page->setTpl("index");
 
+});
+
+// Rota p/ Login
+$app->get('/admin/login', function() {
+
+    $page = new PageAdmin([
+        "header"=>false,
+        "footer"=>false
+    ]);
+
+    $page->setTpl("login");
+
+});
+
+// Rota Validar Login
+$app->post('/admin/login', function() {
+
+    User::login($_POST["login"], $POST["password"]);
+
+    header("Location: /admin");
+    exit;
+});
+
+// Rota p/ Logout
+$app->get('/admin/logout', function() {
+
+    User::logout();
+
+    header("Location: /admin/login");
+    exit;
+    
 });
 
 $app->run();
