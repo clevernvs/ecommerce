8c3752d784730de2610471ecb74b2a5ae80e86f9
Cleverton Neves <c.neves88@gmail.com>
2018-06-26 12:25:40 -0300

    Admin - Categorias

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
 use \Hcode\Model\User;
+use Hcode\Model\Category;
 
 $app = new Slim();
 
@@ -215,7 +216,84 @@ $app->get("/admin/forgot/reset", function (){
 
 });
 
+// Rota p/ CATEGORIAS
+$app->get("/admin/categories", function(){
+    
+    User::verifyLogin();
 
+    $categories = Category::listAll();
+
+    $page = new PageAdmin();
+    $page->setTpl("categories", [
+        'categories'=>$categories,
+    ]);
+
+});
+
+// Rota p/ CRIAR CATEGORIAS
+$app->get("/admin/categories/create", function () {
+    
+    User::verifyLogin();
+
+    $page = new PageAdmin();
+    $page->setTpl("categories-create");
+
+});
+
+// Rota p/ CRIAR CATEGORIAS
+$app->post("/admin/categories/create", function () {
+
+    User::verifyLogin();
+
+    $category = new Category();
+    $category->setData($_POST);
+    $category->save();
+
+    header('Location: /admin/categories');
+    exit;
+});
+
+// Rota p/ DELETAR CATEGORIAS
+$app->get("/admin/categories/:idcategory/delete", function ($idcategory) {
+
+    User::verifyLogin();
+
+    $category = new Category();
+    $category->get((int)$idcategory);
+    $category->delete();
+
+    header('Location: /admin/categories');
+    exit;
+});
+
+// Rota p / ATUALIZAR CATEGORIAS
+$app->get("/admin/categories/:idcategory", function ($idcategory) {
+
+    User::verifyLogin();
+
+    $category = new Category();
+    $category->get((int)$idcategory);
+
+    $page = new PageAdmin();
+    $page->setTpl("categories-update", [
+        'category'=>$category->getValues()
+    ]);
+
+});
+
+// Rota p / ATUALIZAR CATEGORIAS
+$app->post("/admin/categories/:idcategory", function ($idcategory) {
+
+    User::verifyLogin();
+
+    $category = new Category();
+    $category->get((int)$idcategory);
+    $category->setData($_POST);    
+    $category->save();
+
+    header('Location: /admin/categories');
+    exit;
+});
 
 $app->run();
 
