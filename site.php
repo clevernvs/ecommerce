f473247a2fe61e90575767bbdb2daa1e43367d27
Cleverton Neves <c.neves88@gmail.com>
2018-06-30 19:25:06 -0300

    Site - Produtos

--- site.php
+++ site.php
@@ -1,16 +1,17 @@
 <?php
 
 use \Hcode\Page;
+use Hcode\Model\Product;
 
 // Rota p/ o SITE
 $app->get('/', function () {
 
+    $products = new Product();   
+
     $page = new Page();
-    $page->setTpl("index");
-
+    $page->setTpl("index", [
+        'products'=>Product::checkList($products)
+    ]);
 });
-
-
-
 
 ?>
